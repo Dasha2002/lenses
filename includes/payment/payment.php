981991a5c6ad8f5ed04bda35f91b316b95399ed3
Dasha2002 <kosolapova.dasha2018@yandex.ru>
2026-06-19 10:52:22 +0500

    секция оплата

---
+++ includes/payment/payment.php
@@ -0,0 +1,60 @@
+<section class="payment-section">
+    <div class="payment-block">
+        <h2 class="payment-title">Мы предполагаем следующие варианты:</h2>
+
+        <div class="payment-options">
+            <!-- Вариант 1 -->
+            <div class="payment-option">
+                <div class="payment-option__icon">
+                    <img src="/assets/fgs16_delivery.svg">
+                </div>
+                <div class="payment-option__text">
+                    Оплата наличными или картой курьеру
+                </div>
+            </div>
+
+            <!-- Вариант 2 -->
+            <div class="payment-option">
+                <div class="payment-option__icon">
+                    <img src="/assets/fgs16_shop-3.svg">
+                </div>
+                <div class="payment-option__text">
+                    Оплата в салоне оптики при самовывозе
+                </div>
+            </div>
+
+            <!-- Вариант 3 -->
+            <div class="payment-option">
+                <div class="payment-option__icon">
+                    <img src="/assets/fgs16_payment.svg">
+                </div>
+                <div class="payment-option__text">
+                    Оплата банковской картой на сайте<br>
+                    (временно недоступно)
+                </div>
+            </div>
+
+            <!-- Вариант 4 -->
+            <div class="payment-option">
+                <div class="payment-option__icon">
+                    <img src="/assets/fgs16_contract.svg">
+                </div>
+                <div class="payment-option__text">
+                    Оплата по безналичному расчету
+                </div>
+            </div>
+        </div>
+
+        <div class="payment-info">
+            <p class="payment-info__text">
+                Автоматическая бронь не подразумевается, так как не учитывается система складских остатков.
+                Но есть ручная бронь через отслеживание в системе сайта.
+            </p>
+            <p class="payment-info__phone">
+                О возможности резерва товара уточняйте у консультанта по телефону
+                <a href="tel:+78129292958">(812) 929-29-58</a>
+            </p>
+        </div>
+    </div>
+
+</section>

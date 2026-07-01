b3c854d2f1d39845cce31c0008ab25e409fe89a9
Dasha2002 <kosolapova.dasha2018@yandex.ru>
2026-07-01 16:41:15 +0500

    страница аккаунта

---
+++ includes/account/account.php
@@ -0,0 +1,60 @@
+<main class="site-main block_padding product_columns_5">
+    <div class="site-main__inner block_width gr-inner-pages">
+
+        <div class="g-label-tabs g-label-tabs--indents gr-user-tabs">
+            <a class="g-user-profile__item g-user-profile--opened" href="/users?mode=home">Мой кабинет</a>
+            <a class="g-user-profile__item " href="/registration-page.php" style="color: #3F3F3F;">Смена пароля</a> <a href="/users/logout" class="g-button g-user__button g-user__button-account">Выйти</a>
+        </div>
+
+        <div class="user__home-page">
+
+            <div class="account-one-block">
+                <br>Добро пожаловать! Вы авторизовались как ИМЯ.<br><br>
+            </div>
+
+
+            <div class="account-two-block">
+                <b>E-mail:</b> email@yandex.ru <br>
+                <b>Количество бонусов:</b> 0
+            </div>
+
+
+            <div class="account-thre-block">
+                <div class="g-user-info">
+                    <strong>Фамилия:</strong>
+                    <span>
+                        Фамилия
+                    </span>
+                </div>
+                <div class="g-user-info">
+                    <strong>Имя:</strong>
+                    <span>
+                        Имя
+                    </span>
+                </div>
+                <div class="g-user-info">
+                    <strong>Отчество:</strong>
+                    <span>
+                        Отчество
+                    </span>
+                </div>
+                <div class="g-user-info">
+                    <strong>Телефон:</strong>
+                    <span>
+                        +00000000000
+                    </span>
+                </div>
+            </div>
+
+
+
+
+
+
+
+            <div class="g-clear"></div>
+        </div>
+
+    </div>
+
+</main>
